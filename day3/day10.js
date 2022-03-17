8acfbccdf93008da9a115d5ceae379d141131781
Sergeidubouski <sergeidubovskiy1990@gmail.com>
2022-03-17 12:11:30 +0300

    added

--- day3/day10.js
+++ day3/day10.js
@@ -17,3 +17,11 @@ function paperwork(n, m) {
 }
 const opposite = number => -number 
   
+
+
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
