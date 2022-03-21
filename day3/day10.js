e8faf7740f3cf28ab37c82e66a62c12a3424452d
Sergeidubouski <sergeidubovskiy1990@gmail.com>
2022-03-21 12:50:27 +0300

    ineedtrain

--- day3/day10.js
+++ day3/day10.js
@@ -25,3 +25,13 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = ""; // Change this line
+// Only change code above this line
